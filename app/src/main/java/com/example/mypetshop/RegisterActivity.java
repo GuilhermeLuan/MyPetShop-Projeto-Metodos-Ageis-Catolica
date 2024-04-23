f3fc9e22084d6a072ebba01c177d024aad549175
Guilherme Luan <guilhermeluan@pm.me>
2024-04-23 14:06:39 -0300

    feat: seta do header
    seta do header volta pra tela de login agora

--- app/src/main/java/com/example/mypetshop/RegisterActivity.java
+++ app/src/main/java/com/example/mypetshop/RegisterActivity.java
@@ -1,6 +1,12 @@
 package com.example.mypetshop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+    private ImageView imageView_careleft, imageView_show_hide_pwd;
+    private EditText editText_nameRegister, editText_lastNameRegister, editText_CPF,
+            editText_Cellphone, editText_Email, editText_Password;
+
+    private Button bt_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +32,32 @@ public class RegisterActivity extends AppCompatActivity {
             return insets;
         });
 
+        startComponents();
+
+        imageView_careleft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginView();
+            }
+        });
+
+    }
+
+    //Alterar esse parte para quando tiver a tela de login pronta
+    private void loginView(){
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void startComponents(){
+        imageView_careleft = findViewById(R.id.imageView_careleft);
+        imageView_show_hide_pwd = findViewById(R.id.imageView_show_hide_pwd);
+        editText_nameRegister = findViewById(R.id.editText_nameRegister);
+        editText_lastNameRegister = findViewById(R.id.editText_lastNameRegister);
+        editText_CPF = findViewById(R.id.editText_CPF);
+        editText_Cellphone = findViewById(R.id.editText_Cellphone);
+        editText_Email = findViewById(R.id.editText_Email);
+        editText_Password = findViewById(R.id.editText_Password);
+        bt_register = findViewById(R.id.bt_register);
     }
 }
