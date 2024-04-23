0e5bcee5781d9a6cf6788c2bee525183e30650a8
Guilherme Luan <guilhermeluan@pm.me>
2024-04-23 14:34:19 -0300

    feat: campos obrigatórios de registro
    Campos de registro são obrigatórios e feedback em caso de erros.

--- app/src/main/java/com/example/mypetshop/RegisterActivity.java
+++ app/src/main/java/com/example/mypetshop/RegisterActivity.java
@@ -1,12 +1,14 @@
 package com.example.mypetshop;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,12 +16,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class RegisterActivity extends AppCompatActivity {
     private ImageView imageView_careleft, imageView_show_hide_pwd;
     private EditText editText_nameRegister, editText_lastNameRegister, editText_CPF,
             editText_Cellphone, editText_Email, editText_Password;
 
     private Button bt_register;
+
+    String[] mensagens = {
+            "Preencha todos os campos",
+            "Cadastro realizado com sucesso",
+            "A senha deve ter pelo menos 8 caracteres.",
+            "Insira um CPF valido!",
+            "CPF já está registrado!"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +53,66 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        showHidePwd();
+
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText_nameRegister.getText().toString();
+                String lastName = editText_lastNameRegister.getText().toString();
+                String cpf = editText_CPF.getText().toString();
+                String email = editText_Email.getText().toString();
+                String password = editText_Password.getText().toString();
+                String cellphoneNumber = editText_Cellphone.getText().toString();
+
+                if(name.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || cellphoneNumber.isEmpty()){
+                    showSnackbar(v, mensagens[0]);
+                }
+                //Verifico o cpf apenas se tem 11 caracteres, pra facilitar os testes e não ter que usar dados pessoais.
+                else if (cpf.length() < 11) {
+                    showSnackbar(v, mensagens[3]);
+                } else if (password.length() < 8) {
+                    showSnackbar(v, mensagens[2]);
+                } else {
+                    registerUser(v);
+                }
+            }
+        });
+
+    }
+
+    private void registerUser(View v){
+
     }
 
     //Alterar esse parte para quando tiver a tela de login pronta
     private void loginView(){
         Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void showHidePwd(){
+        imageView_show_hide_pwd.setImageResource(R.drawable.ic_eye_closed);
+        imageView_show_hide_pwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText_Password.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    editText_Password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    imageView_show_hide_pwd.setImageResource(R.drawable.ic_eye_closed);
+                }
+                else {
+                    editText_Password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    imageView_show_hide_pwd.setImageResource(R.drawable.ic_eye);
+                }
+            }
+        });
+    }
+
+    private void showSnackbar(View v, String mensagem) {
+        Snackbar snackbar = Snackbar.make(v, mensagem, Snackbar.LENGTH_SHORT);
+        snackbar.setBackgroundTint(Color.WHITE);
+        snackbar.setTextColor(Color.BLACK);
+        snackbar.show();
     }
 
     private void startComponents(){
