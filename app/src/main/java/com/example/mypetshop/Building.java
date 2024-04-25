c84aeafaf475401e67d950efe5522e577095fd89
lizandra.malta <lizlrmalta@gmail.com>
2024-04-25 20:05:45 -0300

    fazendo tela de em construção

--- app/src/main/java/com/example/mypetshop/Building.java
+++ app/src/main/java/com/example/mypetshop/Building.java
@@ -1,8 +1,12 @@
 package com.example.mypetshop;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,32 +15,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class Building extends AppCompatActivity {
 
-    private TextView sign_in_button;
+    private ImageView go_back_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_building);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getColor(R.color.white));
+
         Init();
-        sign_in_button.setOnClickListener(new View.OnClickListener() {
+        go_back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Building.class);
+                Intent intent = new Intent(Building.this, Login.class);
                 startActivity(intent);
             }
         });
-        
     }
 
     private void Init(){
-        sign_in_button = findViewById(R.id.sign_in_button);
+        go_back_button = findViewById(R.id.go_back_button);
     }
 }
